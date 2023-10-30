bf452c0bf11236094fdd803419d62aa06f778907
Maksim <auksinistonic@gmail.com>
2023-10-30 13:30:26 +0100

    C01

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvolkman <mvolkman@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 13:03:52 by mvolkman          #+#    #+#             */
+/*   Updated: 2023/08/22 15:01:51 by mvolkman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	start;
+	int	end;
+
+	start = 0;
+	end = size - 1;
+	while (start < end)
+	{
+		temp = tab[start];
+		tab[start] = tab[end];
+		tab[end] = temp;
+		start++;
+		end--;
+	}
+}
